5bc8d4e9e1ca6e94b9f046bae6b254efa8015dad
VNTRUNGQUOC <ultramodifier123456@gmail.com>
2023-12-07 21:44:04 +0700

    update

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded', function () {
         const randomKey = generateRandomKey();
         keyOutput.value = randomKey;
         generateBtn.style.display = 'none';
-        keyOutput.style.height = '150px';
-        keyOutput.style.marginTop = '0';
+        keyOutput.style.height = '150px'; // Adjust the height as needed
+        keyOutput.style.marginTop = '0'; // Remove the top margin
     });
 
     function generateRandomKey() {
         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        const keyLength = 16;
+        const keyLength = 20;
         let randomKey = 'Creal - ' + '';
 
         for (let i = 0; i < keyLength; i++) {
